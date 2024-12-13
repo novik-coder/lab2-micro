620b2bc737b17a18b52c5d4ab6e8749e500ea3d1
gaolei <gaoleigaodazhuang@126.com>
2024-12-13 15:46:39 +0800

    服务代码

---
+++ reservation-servcie/src/main/java/com/study/client/RatingServiceClient.java
@@ -0,0 +1,12 @@
+package com.study.client;
+
+import com.study.common.dto.rating.UserRatingResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(name = "rating-service")
+public interface RatingServiceClient {
+    @GetMapping("/api/v1/rating")
+    UserRatingResponse getUserRating(@RequestHeader("X-User-Name") String username);
+}
